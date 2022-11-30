9a7398a216aa496ac672267cb8b153bdd266e1eb
Richa dwivedi <richa_dwivedi@mckisney.com>
2022-11-30 10:42:51 +0000

    [Richa] Add Sorting Grouping and Filtering

--- project_es5/webapp/controller/View1.controller.js
+++ project_es5/webapp/controller/View1.controller.js
@@ -1,22 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "../model/formatter" //doubt custom formatter not working
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "../model/formatter"
+
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast,formatter) {
+    function (Controller, MessageToast, Filter, FilterOperator, formatter) {
         "use strict";
 
         return Controller.extend("projectes5.controller.View1", {
-   // onInit: function () {
-    
+            // onInit: function () {
+
             // },
 
-            formatter : formatter,
-            onShowHello: function(){
+            formatter: formatter,
+            onShowHello: function () {
                 // MessageToast.show("Hello!! This is my first UI5 project");
 
                 var oBundle = this.getView().getModel("i18n").getResourceBundle();
@@ -25,6 +28,24 @@ sap.ui.define([
                 // show message
                 MessageToast.show(sMsg);
 
+            },
+
+            onFilterProducts: function (oEvent) {
+
+                // build filter array, query from search box
+                var aFilter = [], sQuery = oEvent.getParameter("query"),
+
+                    // retrieve list control, whole list
+                    oList = this.getView().byId("list0"),
+                    // get binding for aggregation 'items'
+                    oBinding = oList.getBinding("items");
+                if (sQuery) {
+                    aFilter.push(new Filter("ProductID", FilterOperator.Contains,
+                        sQuery)); //doubt what's happening in this line?
+                }
+                // apply filter. an empty filter array simply removes the filter
+                // which will make all entries visible again
+                oBinding.filter(aFilter);
             }
         });
     });
