f4f8f04df58fbdfaeef405d2d75417d53b018296
Richa dwivedi <richa_dwivedi@mckisney.com>
2022-12-05 13:52:18 +0000

    Completed Assignment 1

--- assignment1/webapp/controller/detail.controller.js
+++ assignment1/webapp/controller/detail.controller.js
@@ -1,5 +1,28 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], 
+function (Controller) {
     "use strict";
 
-    return Controller.extend("com.assignment1.assignment1.controller.detail", { });
+    return Controller.extend("assignment1.controller.detail", { 
+        /**
+         * After calling navTo, the hash of the browser is updated & will get an event on the DetailController
+         * here we will extract the objectId using the Event.getParameter function, then will bind the data to the view:
+         */
+
+         onInit: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("detail").attachMatched(this._onObjectMatched, this);
+          },
+          _onObjectMatched: function (oEvent) {
+            /**
+             * oEvent.getParameter(“arguments”): gets navigation
+             */
+            this.getView().bindElement({
+                path: "/Products(" + oEvent.getParameter("arguments").productId + ")"
+            });
+
+          }
+      
+    });
 });
--- assignment1/webapp/controller/master.controller.js
+++ assignment1/webapp/controller/master.controller.js
@@ -12,12 +12,32 @@ sap.ui.define([
 
             // }
 
+            // 1. First we have to implement a press handler on the ListItem
             onClickListItem: function(oEvent){
-                var oRouter=sap.ui.UIComponent.getRouterFor(this);
-                var productId=oEvent.getSource.getBindingContext.getProperty("ProductID");
-                oRouter.navTo("detail",{
-                   selected_productId : productId
-                });
+
+                 // 2. we will extract the current ID of the object pressed by the user on the list by using its bindingContext.
+                 
+                  //3. getSource: Returns the event provider on which the event was fired.
+            
+                 var clickedProductId=oEvent.getSource().getBindingContext().getProperty("ProductID");
+                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+                 /** Since we want to navigate to the “object” route, 
+                  * we need to supply the mandatory objectId parameter
+                  * pass it to the navTo function: Navigates to a specific route defining a set of parameters
+                  * The source is the list item that got pressed
+                 */
+
+                  //Routing :SAPUI5 offers hash-based navigation, 
+                
+                 oRouter.navTo("detail", {
+                    productId:clickedProductId
+                 });
+
+                // code to open one simple Target page
+                // oRouter.getTargets().display("Targetdetail");
             }
+
+
         });
     });
